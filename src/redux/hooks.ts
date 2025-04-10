899a5310a364f3ebbe3de055de21a5d560f143fd
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2025-04-09 22:44:28 -0300

    added: @types

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -2,4 +2,4 @@ import { useDispatch, useSelector } from "react-redux";
 import type { RootState, AppDispatch } from "./store";
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useDispatch.withTypes<RootState>()
+export const useAppSelector = useSelector.withTypes<RootState>()

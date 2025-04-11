62ddd447bfd9f4186b41b065c46fae82c670fe3d
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2025-04-11 16:23:13 -0300

    useAuth Hook

---
+++ src/hooks/auth.ts
@@ -0,0 +1,81 @@
+import { User } from "../@types/Auth"
+import { useAppDispatch } from "../redux/hooks"
+import { setAuthStatus, setAuthToken, setUser } from "../redux/slices/authSlice"
+import { getUser, signIn, signUp } from "../services/requests"
+
+const LOCAL_STORAGE_KEY = import.meta.env.VITE_LOCAL_STORAGE_AUTH_KEY
+
+export const useAuth = () => {
+
+    const dispatch = useAppDispatch()
+
+    // Function to authenticate the user
+    const authenticate = (user: User, authToken: string) => {
+        dispatch(setUser(user))
+        dispatch(setAuthToken(authToken))
+        dispatch(setAuthStatus('authenticated'))
+
+        localStorage.setItem(LOCAL_STORAGE_KEY, authToken)
+    }
+
+    // Function to get token from local storage
+    const handleGetToken = () => localStorage.getItem(LOCAL_STORAGE_KEY)
+
+    // Get the user using the authToken saved in Local Storage
+    const handleAuthenticateUser = async() => {
+        const request = await getUser()
+        const authToken = handleGetToken()
+
+        if(!request.data || !authToken){
+            dispatch(setAuthStatus('not_authenticated'))
+            return;
+        }
+
+        const { data } = request;
+        authenticate(data.user, authToken)
+    }
+
+    // Function to signIn
+    const handleSignIn = async({email, password}: {email: string, password: string}) => {
+        const request = await signIn(email, password)
+
+        if(request.data){
+            const { data } = request;
+            authenticate(data.user, data.authToken) 
+            return true;
+        }
+
+        dispatch(setAuthStatus('not_authenticated'))
+        return request.error
+    }
+
+    const handleSignUp = async({name, email, password}: {name: string,email: string, password: string}) => {
+        const request = await signUp(name, email, password)
+
+        if(request.data){
+            const { data } = request;
+            authenticate(data.user, data.authToken) 
+            return true;
+        }
+
+        dispatch(setAuthStatus('not_authenticated'))
+        return request.error
+    }
+
+    // Function to signOut
+    const handleSignOut = () => {
+        dispatch(setUser(null))
+        dispatch(setAuthToken(null))
+        dispatch(setAuthStatus('not_authenticated'))
+
+        localStorage.removeItem(LOCAL_STORAGE_KEY)
+    }
+
+    return {
+        handleGetToken,
+        handleAuthenticateUser,
+        handleSignIn,
+        handleSignUp,
+        handleSignOut
+    }
+}
